23af5b5d09e1c24a63ef7d1654a9c512b975c0f1
Shreya Bhattacharya <62888347+shreyab375@users.noreply.github.com>
2024-10-07 12:48:29 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -174,8 +174,9 @@ st.image('Images/departure arrival.png', caption="Departure vs. Arrival Delay Pa
 # 7. Predictive Modeling for ETCS Impact
 st.header("7. Predictive Modeling for ETCS Impact")
 st.markdown("""
-Using predictive modeling, it was found that having ETCS has an **significant impact (of approximately 27%)** on predicting delay times. (Technical details: R-squared = 0.58).
-This indicates that ETCS contributes significantly to the ability to forecast train delays.
+Using predictive modeling, it was found that having ETCS has an **significant impact ** on predicting delay times. This means that having ETCS helps us better anticipate delays, aiding train operators in improving schedules and punctuality.
+(Technical Justification:
+ETCS significantly impacts delay predictions, explaining 27% of the variance in delays (R-squared = 0.58). This indicates that while other factors affect delays, ETCS plays a crucial role in enhancing predictive accuracy.
 """)
 
 # Insert figure for predictive modeling
